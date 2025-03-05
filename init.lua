0a56803b8055632e2af30de78a765fd08afff649
deruta <deruta.fuzzy377@gmail.com>
2025-03-05 12:37:47 -0500

    update mason `ensure_installed` list
    Add language servers, formatters and linters:
    
    - `lua-language-server`
    - `gopls`
    - `deno`
    - `vtsls`
    - `tailwindcss-language-server`
    - `prettier`
    - `markdownlint`

--- init.lua
+++ init.lua
@@ -734,6 +734,13 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'lua-language-server',
+        'gopls',
+        'deno',
+        'vtsls',
+        'tailwindcss-language-server',
+        'prettier',
+        'markdownlint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
